84ca71645ee6024e86b6945ef9a90ea031f27733
l.liu <lidan.liu@net-a-porter.com>
2015-11-01 11:44:04 +0000

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+	var banner = '/*n<%= pkg.name %> <%= pkg.version %>';
+		banner += ' - <%= pkg.description %>n<%= pkg.repository.url %>';
+		banner += 'nBuilt on <%= grunt.template.today("yyyy-mm-dd") %>n*/n';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			files: ['gruntfile.js','src/*.js'],
+			options: {
+				maxlen: 80,
+				quotmark: 'single'
+			}
+		},
+		concat: {
+			options: {
+				seperator: ';n',
+				banner: banner
+			},
+			build: {
+				files: [{
+					src: ['src/*.js'],
+					dest: 'build/<%= pkg.name %>.js'	
+				}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('default',['jshint','concat']);
+};
